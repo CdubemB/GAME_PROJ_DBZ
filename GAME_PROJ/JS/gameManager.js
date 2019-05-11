3aacddcc149166737f645f13b82efc286408abe2
CdubemB <dubemwb@gmail.com>
2019-05-11 15:55:04 +0100

    started working on setPreFight function

--- GAME_PROJ/JS/gameManager.js
+++ GAME_PROJ/JS/gameManager.js
@@ -15,13 +15,15 @@ let GameManager = {
         player = new Player(character, 80, 0, 50, 200, 50);
         break;
       case "Trunks":
-        player = new Player(character, 200, 0, 200, 100, 50);
+        player = new Player(character, 200, 0, 200, 80, 70);
         break;
     }
     let getInterface = document.querySelector(".interface");
-    getInterface.innerHTML = "<img src='Assets/" + character + ".jpg' class='img-avatar'><div><h3>"+ character +"</h3><p>HEALTH:  "+ player.health + " </p></div>";
+    getInterface.innerHTML = "<img src='Assets/" + character + ".jpg' class='img-avatar'><div><h3>"+ character +"</h3><p>HEALTH:  "+ player.health + " </p><p>KI:  "+ player.ki + " </p><p>STRENGTH:  "+ player.strength + " </p><p>AGILITY:  "+ player.agility + " </p><p>SPEED:  "+ player.speed+ " </p></div>"; //Adding a section where it shows character along with its stats
   },
   setPreFight: function(){
+
+    let getHeader;
 
   }
 }
