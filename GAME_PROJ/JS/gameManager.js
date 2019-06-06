248c41076f7c582e1dbf62c0cd7e71e48e0fdd99
CdubemB <dubemwb@gmail.com>
2019-06-06 21:06:15 +0100

    almost finished base game (still one bug to fix)

--- GAME_PROJ/JS/gameManager.js
+++ GAME_PROJ/JS/gameManager.js
@@ -12,14 +12,14 @@ let GameManager = {
         player = new Player(character, 100, 0, 100, 150, 200);
         break;
       case "Frieza":
-        player = new Player(character, 80, 0, 50, 200, 50);
+        player = new Player(character, 80, 200, 50, 200, 50);
         break;
       case "Trunks":
         player = new Player(character, 200, 0, 200, 80, 70);
         break;
     }
     let getInterface = document.querySelector(".interface");
-    getInterface.innerHTML = "<img src='Assets/" + character + ".jpg' class='img-avatar'><div><h3>"+ character +"</h3><p>HEALTH:  "+ player.health + " </p><p>KI:  "+ player.ki + " </p><p>STRENGTH:  "+ player.strength + " </p><p>AGILITY:  "+ player.agility + " </p><p>SPEED:  "+ player.speed+ " </p></div>"; //Adding a section where it shows character along with its stats
+    getInterface.innerHTML = "<img src='Assets/" + character + ".jpg' class='img-avatar'><div><h3>"+ character +"</h3><p class='health-player'>HEALTH:  "+ player.health + " </p><p>KI:  "+ player.ki + " </p><p>STRENGTH:  "+ player.strength + " </p><p>AGILITY:  "+ player.agility + " </p><p>SPEED:  "+ player.speed+ " </p></div>"; //Adding a section where it shows character along with its stats
   },
   setPreFight: function(){
 
@@ -54,5 +54,7 @@ let GameManager = {
    getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()"> Attack!</a>';
    getEnemy.innerHTML = '<img src="Assets/'+  enemy.enemyType + '.jpg" alt=" '+ enemy.enemyType +'" class="img-avatar"><div><h3>' + enemy.enemyType +  ' </h3><p class="health-enemy">Health: ' + enemy.health + '</p> <p class="ki-enemy">Ki: ' + enemy.ki + '</p><p class="strength-enemy">Strength: ' + enemy.strength + '</p><p class="agility-enemy">Agility: ' + enemy.agility + '</p><p class="health-enemy">Speed: ' + enemy.speed + ' </p></div> ';
   } 
+  //Initiate attack order
+
 }
  
--- GAME_PROJ/JS/player.js
+++ GAME_PROJ/JS/player.js
@@ -8,3 +8,79 @@ function Player(character, health, ki, strength, agility, speed){
   this.agility = agility;
   this.speed = speed;
 }
+
+let PlayerMoves = {
+  calcAttack: function(){
+    //determins the first attacker
+    let getPlayerSpeed = player.speed;
+    let getEnemySpeed = enemy.speed;
+    //player will attack
+  let playerAttack = function(){
+    let calcBaseDamage;
+
+    if (player.ki > 0){
+      calcBaseDamage = player.strength * player.ki / 1000;
+    }
+    else{
+      calcBaseDamage = player.strength * player.agility / 1000;
+    }
+    let offsetDamage = Math.floor(Math.random * Math.floor(10));
+    let calcOutputDamage = calcBaseDamage + offsetDamage;
+    //The amount of attacks
+    let numberOfHits = Math.floor(Math.random * Math.floor(player.agility / 10) / 2 + 1);
+    
+    let attackValues = [calcOutputDamage, numberOfHits];
+    return attackValues;
+  }
+  //Enemy will attack
+  let enemyAttack = function(){
+    let calcBaseDamage;
+
+    if (enemy.ki > 0){
+      calcBaseDamage = enemy.strength * enemy.ki / 1000;
+    }
+    else{
+      calcBaseDamage = enemy.strength * enemy.agility / 1000;
+    }
+    let offsetDamage = Math.floor(Math.random * Math.floor(10));
+    let calcOutputDamage = calckBaseDamage + offsetDamage;
+    //The amount of attacks
+    let numberOfHits = Math.floor(Math.random * Math.floor(enemy.agility / 10) / 2 + 1);
+    
+    let attackValues = [calcOutputDamage, numberOfHits];
+    return attackValues;
+  }
+  //getting the player and enemy's health
+  let getPlayerHealth = document.querySelector('.health-player');
+  let getEnemyHealth = document.querySelector('.health-enemy');
+  //Initiate atack order
+  if (getPlayerSpeed >= getEnemySpeed){
+    let playerAttackValues = playerAttack();
+    let totalDamage = playerAttackValues[0] * playerAttackValues[1];
+    enemy.health = enemy.health - totalDamage;
+    alert ("you hit" + playerAttackValues[0] +"damage"+playerAttackValues[1]+"times");
+    if (enemy.health <= 0){
+      alert("you win! if you want to play again refresh the browser");
+      getPlayerHealth.innerHTML = "HEALTH:" + player.health;
+      getEnemyHealth.innerHTML = "HEALTH: 0";
+    }
+    else {
+      getEnemyHealth.innerHTML = "HEALTH:" + enemy.health;
+      //Enemy will attack
+      let enemyAttackValues = enemyAttack();
+      let totalDamage = enemyAttackValues[0] * enemyAttackValues[1];
+      player.health = player.health - totalDamage;
+      alert ("Enemy hit"+enemyAttackValues[0]+"damage"+enemyAttackValues[1]+"times");
+      if (player.health <= 0){
+        alert("you win! if you want to play again refresh the browser");
+        getPlayerHealth.innerHTML = "HEALTH: 0";
+        getEnemyHealth.innerHTML = "HEALTH:" + enemy.health;
+      }
+      else{
+        getPlayerHealth.innerHTML = "HEALTH:" + player.health;
+      }
+    }
+  }  
+  }
+   
+}
